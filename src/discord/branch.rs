3d3a2dfeebf936e7f5dfa52cef05f1ba92ca1848
RGBCube <RGBCube@users.noreply.github.com>
2023-05-20 19:00:43 +0300

    Apply clippy lints

--- src/discord/branch.rs
+++ src/discord/branch.rs
@@ -1,12 +1,8 @@
-use std::path::PathBuf;
+use std::path::Path;
 
-const BRANCHES: [&str; 3] = [
-    "pbt",
-    "canary",
-    "development",
-];
+const BRANCHES: [&str; 3] = ["pbt", "canary", "development"];
 
-pub fn get_branch(path: &PathBuf) -> &'static str {
+pub fn get_branch(path: &Path) -> &'static str {
     let name = path.file_name().unwrap().to_string_lossy();
 
     for branch in BRANCHES {
