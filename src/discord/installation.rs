c7389277e46faaaaa27211d3e8736d0590e19e41
RGBCube <RGBCube@users.noreply.github.com>
2023-05-20 19:00:34 +0300

    Add test program

--- src/discord/installation.rs
+++ src/discord/installation.rs
@@ -1,9 +1,10 @@
 use std::path::PathBuf;
 
+#[derive(Debug)]
 pub struct Installation {
-    pub path: PathBuf, // Base path.
+    pub path: PathBuf,     // Base path.
     pub app_path: PathBuf, // Path to the app folder.
-    pub branch: String, // "stable", "ptb", "canary", "development", ...
+    pub branch: String,    // "stable", "ptb", "canary", "development", ...
     pub is_patched: bool,
     pub is_sys_electron: bool,
     pub is_openasar: bool,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod discord;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let _found_installations = discord::find()?;
+
+    for installation in _found_installations {
+        println!("{:?}", installation);
+    }
+
+    Ok(())
 }
