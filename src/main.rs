4b45b15d5c015fe0c2c6e67c3ae7ef79b0f46cd9
RGBCube <RGBCube@users.noreply.github.com>
2023-05-27 15:20:56 +0300

    Print found installtions directly instead of iterating in install script

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod discord;
 
 fn main() -> anyhow::Result<()> {
-    let _found_installations = discord::find()?;
+    let found_installations = discord::find()?;
 
-    for installation in _found_installations {
-        println!("{:?}", installation);
-    }
+    println!("{:?}", found_installations);
 
     Ok(())
 }
